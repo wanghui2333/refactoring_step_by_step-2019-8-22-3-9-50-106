d51f646ed985dd6ca347cb083e2519cc32e42361
wanghui <793147654@qq.com>
2019-08-27 10:50:13 +0800

    代码重构完成

--- src/main/java/com/tws/refactoring/Driver.java
+++ src/main/java/com/tws/refactoring/Driver.java
@@ -1,9 +1,23 @@
 package com.tws.refactoring;
 
+/**
+ * 功能描述 : Driver类
+ * @Author : 王辉
+ * @Email : wanghui16@zybank.com.cn
+ * @Date : 2019-08-27 10:48
+ */
 public class Driver {
-    public int age;
+    private int age;
 
     public Driver(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
     }
 }
--- src/main/java/com/tws/refactoring/Police.java
+++ src/main/java/com/tws/refactoring/Police.java
@@ -1,8 +1,13 @@
 package com.tws.refactoring;
 
+/**
+ * 功能描述 : Police类
+ * @Author : 王辉
+ * @Email : wanghui16@zybank.com.cn
+ * @Date : 2019-08-27 10:48
+ */
 public class Police {
     public boolean checkDriver(Driver driver) {
-        if(driver.age >= 18) return true;
-        else return false;
+        return driver.getAge() >= 18;
     }
 }
